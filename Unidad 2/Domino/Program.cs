d404f630d32f7a0dc87d40669c6f8f83d8b5db6d
Marco Reyes <54376229+marcoreyes666@users.noreply.github.com>
2019-10-22 20:56:45 -0500

    ...

---
+++ Unidad 2/Domino/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace domino
+{
+class Domino
+{
+    public readonly int num;
+    public readonly int den;
+    public Domino (int numerador, int denominador)
+    {
+        if (denominador == 0)
+        {
+            throw new ArgumentException ("No Cero");
+        }
+        num = numerador;
+        den=denominador;
+    }
+    public static int operator + (Domino a, Domino b)
+    {
+        return (a.num + b.den + b.num + a.den + b.den);
+    }
+    public static Domino operator (Domino a, Domino b)
+    {
+       Domino (a.num, a.den);
+    }
+}
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Domino a=new Domino (6,2);
+            Domino b=new Domino (3,3);
+            Console.WriteLine();
+        }
+    }
+}
